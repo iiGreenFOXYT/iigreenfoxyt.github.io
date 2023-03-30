1828b4d76d4f472a22b37e50664787b4153ba266
iiGreenFOXYT <karemyossry147@gmail.com>
2023-03-30 03:36:24 +0200

    test

--- firebase.js
+++ firebase.js
@@ -1,8 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -12,11 +10,10 @@ const firebaseConfig = {
   projectId: "personal-website-e1e4c",
   storageBucket: "personal-website-e1e4c.appspot.com",
   messagingSenderId: "1067002641079",
-  appId: "1:1067002641079:web:cdb9bf0a48d97af82aeb32",
-  measurementId: "G-VTN4TWWMZ0"
+  appId: "your-app-id",
+  measurementId: "1:1067002641079:web:cdb9bf0a48d97af82aeb32"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-firebase.initializeApp(firebaseConfig);
