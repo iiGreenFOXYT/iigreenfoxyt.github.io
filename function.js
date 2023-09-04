859bfbe0a6d89fd4564658812ecabe7c72b2283d
iiGreenFOXYT <karemyossry147@gmail.com>
2023-09-04 03:01:35 +0300

    new

--- function.js
+++ function.js
@@ -16,3 +16,14 @@
       });
     }); 
   })(jQuery);
+
+
+
+  window.addEventListener('scroll', function() {
+    var header = document.querySelector('.nav-bar');
+    if (window.scrollY > 0) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
